0e2e7d34f6937ef78922943268a07afd98960f9e
Fukumu Tsutsumi <levelfourslv@gmail.com>
2017-04-01 12:17:28 +0900

    add batch scripts

---
+++ scripts/pumil_tiger.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+DATASET="tiger"
+N=50
+
+experiment () {
+    prior=$1
+    echo $DATASET $prior
+    for i in $(seq 1 $N); do
+        LOG=".results/pumil_${DATASET}_${prior}.log"
+        (python pumil.py --dataset $DATASET --prior $prior >> $LOG) &
+    done
+    wait
+}
+
+experiment 0.1
+experiment 0.4
+experiment 0.7
---
+++ scripts/puskc_musk2.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+DATASET="musk2"
+N=50
+
+experiment () {
+    prior=$1
+    for i in $(seq 1 $N); do
+        echo $DATASET $prior $i
+        LOG=".results/puskc_${DATASET}_${prior}.log"
+        (python puskc.py --dataset $DATASET --prior $prior >> $LOG)
+    done
+}
+
+experiment 0.1
+experiment 0.4
+experiment 0.7
