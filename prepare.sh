ac4dd22d29c79dd9726c613b649327bfb4389d76
levelfour <levelfourslv@gmail.com>
2017-09-20 11:21:14 +0200

    do not clean

--- prepare.sh
+++ prepare.sh
@@ -8,11 +8,3 @@ sh download_dataset.sh fox
 sh download_dataset.sh tiger
 echo "Generating datasets ..." 2>&1
 python generate_dataset.py
-echo "Cleaning ..." 2>&1
-rm musk1.data
-rm musk2.data
-rm elephant.data
-rm fox.data
-rm tiger.data
-rm -r MilData/
-rm MIL-Data-2002-Musk-Corel-Trec9.tgz
