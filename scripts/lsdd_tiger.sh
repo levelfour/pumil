1d41f9fa348a63c1740c57df839215d6615e5d91
Fukumu Tsutsumi <levelfourslv@gmail.com>
2017-10-08 22:55:18 +0900

    add scripts for LSDD and DSDD

---
+++ scripts/lsdd_tiger.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+DATASET="tiger"
+MODEL="lsdd"
+N=20
+
+experiment () {
+    prior=$1
+    for i in $(seq 1 $N); do
+        echo $DATASET $prior $i
+        LOG=".results/${MODEL}_${DATASET}_${prior}.log"
+        (python ${MODEL}.py --dataset $DATASET --prior $prior >> $LOG)
+    done
+}
+
+experiment 0.1
+experiment 0.2
+experiment 0.3
+experiment 0.4
+experiment 0.5
+experiment 0.6
+experiment 0.7
